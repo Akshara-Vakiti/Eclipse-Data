e1a0c0728424e238160f3267f91bd3f856dba49e
Akshara-Vakiti <aksharavakiti96@gmail.com>
2025-04-23 21:02:31 +0530

    We are pushing the project into central

---
+++ src/eclipsetocentral/e2c.java
@@ -0,0 +1,16 @@
+package eclipsetocentral;
+
+public class e2c 
+{
+	public void swapnumbers()
+	{
+		int a=20,b=5;
+		System.out.println("Before swapping values of a and b:"+a+" "+b);
+		a=a*b;
+		b=a/b;
+		a=a/b;
+		System.out.println("After swapping values of a and b:"+a+" "+b);
+		
+	}
+
+}
