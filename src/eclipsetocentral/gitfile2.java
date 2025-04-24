bb1cf578428e9fccbb3b50cae979ff385111d144
Akshara-Vakiti <aksharavakiti96@gmail.com>
2025-04-24 20:37:16 +0530

    Create gitfile2.java

---
+++ src/eclipsetocentral/gitfile2.java
@@ -0,0 +1,18 @@
+public class gitfile2
+{
+  public void assignmentoperator()
+	{
+		int a=5;
+		a*=5;
+		System.out.println(a);
+		
+		int b=10;
+		b/=5;
+		System.out.println(b);
+		
+		int c=20;
+		c%=5;
+		System.out.println(c);
+		
+	}
+}
